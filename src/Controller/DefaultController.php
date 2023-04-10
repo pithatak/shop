5737b738a26c50e91b9b386853d028d6c2c68aa0
illia <poseva41@gmail.com>
2023-04-10 23:05:15 +0200

    2 ==> comit <== Вюшка для продуктов, недоделаный поиск по категориям.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,22 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Product;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Category;
 
 #[Route('/')]
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_main_index')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/', name: 'app_main_index', methods: ['GET'])]
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository ): Response
     {
 
-        $entityManager = $doctrine->getManager();
-        $product = $entityManager->getRepository(Product::class)->findAll();
+        dump($categoryRepository->findAll() );
+        dump("darowa");
 
         // this looks exactly the same
-        return $this->render('main/main.html.twig', ['products' => $product] );
+        return $this->render('main/index.html.twig', [
+            'products' => $productRepository->findAll(),
+            'categories' => $categoryRepository->findAll()
+        ]);
     }
 }
