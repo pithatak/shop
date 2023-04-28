201af9acefb2b1ad6a2873d104e768b458b313c3
illia <poseva41@gmail.com>
2023-04-28 13:17:15 +0200

    3==> comit <== Поісковик на каждой станічкі, регестрація і логін, доступ,

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Form\SearchForm;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,28 +12,29 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_main_index', methods: ['GET'])]
-    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository, Request $request): Response
+    public function index(Request $request, ProductRepository $productRepository, CategoryRepository $categoryRepository): Response
     {
 
-        $form = $this->createForm(SearchForm::class);
         $search = null;
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($request->query->get('search')) {
 
-            $searchN = $productRepository->findByName($form->getData());
-            $searchD = $productRepository->findByDescription($form->getData());
-            $search['Word'] = $form->getData()['search'];
+            $search['Word'] = $request->query->get('search');
+            $searchN = $productRepository->findByName($search['Word']);
+            $searchD = $productRepository->findByDescription($search['Word']);
             $search['Object'] = array_unique(array_merge($searchN, $searchD), SORT_REGULAR);
-
         }
 
         return $this->render('main/index.html.twig', [
             'productRepository' => $productRepository,
             'categoryRepository' => $categoryRepository,
-            'form' => $form->createView(),
             'search' => $search
         ]);
     }
 
+    #[Route('/search', name: 'app_main_search', methods: ['POST'])]
+    public function searchAction(Request $request)
+    {
+        return $this->redirectToRoute('app_main_page', [ 'search' => $request->request->get('search')]);
+    }
 }
